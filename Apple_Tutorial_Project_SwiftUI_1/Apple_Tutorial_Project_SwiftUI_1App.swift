ee1f6b55948b1253b45b9b662809d3cb0f0f7391
Makan <makanfofana246@gmail.com>
2020-10-23 22:07:37 -0400

    Initial Commit

---
+++ Apple_Tutorial_Project_SwiftUI_1/Apple_Tutorial_Project_SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Tutorial_Project_SwiftUI_1App.swift
+//  Apple_Tutorial_Project_SwiftUI_1
+//
+//  Created by Makan Fofana on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Tutorial_Project_SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
