5d449f5978b4fafaa7626b38bd3161ecca2da336
Makan <makanfofana246@gmail.com>
2020-10-23 23:01:26 -0400

    Apple project 1

--- Apple_Tutorial_Project_SwiftUI_1/ContentView.swift
+++ Apple_Tutorial_Project_SwiftUI_1/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            Text("Joshua Tree National Park")
+                .font(.subheadline)
+        }
     }
 }
 
