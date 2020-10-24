10223336011f2255a7297ba954ca54eacea98c24
Makan <makanfofana246@gmail.com>
2020-10-24 13:00:03 -0400

    Created a Custom Image View

---
+++ Apple_Tutorial_Project_SwiftUI_1/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Apple_Tutorial_Project_SwiftUI_1
+//
+//  Created by Makan Fofana on 10/24/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
